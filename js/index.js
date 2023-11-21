140fb04ee45d45421a3fa1da7e4dc3618087577e
uwebury <142432719+uwebury@users.noreply.github.com>
2023-11-21 16:19:39 +0100

    Calculated and logged the age-badness-sum

--- js/index.js
+++ js/index.js
@@ -8,6 +8,12 @@ form.addEventListener("submit", (event) => {
   const formData = new FormData(event.target);
   const data = Object.fromEntries(formData);
 
+  const firstName = event.target.firstName.value;
+  const ageBadnessSum =
+    Number(event.target.age.value) + Number(event.target.badness.value);
+
+  console.log("The age-badness-sum of", firstName, "is", ageBadnessSum);
+
   console.log(data);
   event.target.reset();
   event.target.elements.firstName.focus();
