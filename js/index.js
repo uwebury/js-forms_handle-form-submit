58dadc09f1b2887a556c4f91183400b10c777c35
uwebury <142432719+uwebury@users.noreply.github.com>
2023-11-21 16:02:04 +0100

    Handled form submit

--- js/index.js
+++ js/index.js
@@ -1,3 +1,14 @@
 console.clear();
 
 const form = document.querySelector('[data-js="form"]');
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const formData = new FormData(event.target);
+  const data = Object.fromEntries(formData);
+
+  console.log(data);
+  event.target.reset();
+  event.target.elements.firstName.focus();
+});
